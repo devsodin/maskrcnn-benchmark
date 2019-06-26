9595009cc904d4a9ce442d093b9618605129f646
Yael Tudela <devsodin@gmail.com>
2019-06-26 13:17:59 +0200

    dataset as cocoDS. WIP

--- tools/dataset_to_coco.py
+++ tools/dataset_to_coco.py
@@ -1,15 +1,19 @@
 import os
 from PIL import Image
 import numpy as np
-import pycococreatortools
+
 import datetime
 import json
+from pycocotools import mask
+from skimage import measure
+
 
 # ROOT_DIR = "C:\\Users\\Yael\\Desktop\\CVC-VideoClinicDBtrain_valid"
-ROOT_DIR = "/home/devsodin/Downloads/MaskRCNN/CVC-VideoClinicDBtrain_valid"
+# ROOT_DIR = "/home/devsodin/Downloads/MaskRCNN/CVC-VideoClinicDBtrain_valid"
+ROOT_DIR = "c:\\Users\\Yael\\Desktop\\CVC-VideoClinicDBtrain_valid"
+MASK_EXTENSION = "_polyp"
 IMAGES_DIR = os.path.join(ROOT_DIR, "images")
 ANNOTATIONS_DIR = os.path.join(ROOT_DIR, "masks")
-MASK_EXTENSION = "_polyp"
 
 INFO = {
     "description": "CVC-CLINIC Dataset",
@@ -27,11 +31,8 @@ LICENSES = [
     }
 ]
 
-#IMAGES
-    #id, license, cocourl, flickrurl, width, height, filenmae, date_captured
-
 # ANNOTATIONS
-    # id, category id, iscrowd, segmentation, image_id, area, bbox(x,y,w,h)
+# id, category id, iscrowd, segmentation, image_id, area, bbox(x,y,w,h)
 
 CATEGORIES = [
     {
@@ -43,21 +44,24 @@ CATEGORIES = [
 ]
 
 
-def generate_bboxes(mask_folder):
-    for file in (os.path.join(mask_folder, file) for file in os.listdir(mask_folder)):
+def generate_bbox(mask):
+    """
 
-        im = Image.open(file)
+    :rtype: list
+    """
+    coors = np.where(np.array(mask) == 1)
 
-        coors = np.where(np.array(im) == 1)
+    if len(coors[0]) == 0:
+        return None
+    else:
+        x_min = np.min(coors[0])
+        x_max = np.max(coors[0])
+        y_min = np.min(coors[1])
+        y_max = np.max(coors[1])
+        w = x_max - x_min
+        h = y_max - y_min
 
-        if len(coors[0]) == 0:
-            pass
-        else:
-            x_min = np.min(coors[0])
-            x_max = np.max(coors[0])
-            y_min = np.min(coors[1])
-            y_max = np.max(coors[1])
-            print(x_min, x_max, y_min, y_max)
+        return [x_min, y_min, w, h]
 
 
 def get_mask_images(mask_file):
@@ -67,45 +71,95 @@ def get_mask_images(mask_file):
     return filter(is_annot_from_image, os.listdir(ANNOTATIONS_DIR))
 
 
-def cocoize_dataset():
+def get_image_coco_info(image_id, filename, size, license="", cocourl="", flickurl="", datacaptured=""):
+    info =  {
+        "id": image_id,
+        "license": license,
+        "cocourl": cocourl,
+        "flickrurl": flickurl,
+        "width": size[0],
+        "height": size[1],
+        "filename": filename,
+        "date_captured": datacaptured
+    }
+    print(info)
+    return info
+
+
+def get_annotations_coco_image(segmentation_id, image_id, category_info, binary_mask):
+    bbox = generate_bbox(binary_mask)
+    segmentation = measure.find_contours(binary_mask,0.5)
+    area = mask.area(mask.encode(np.asfortranarray(binary_mask.astype(np.uint8))))
+    iscrowd = 1 if category_info["is_crowd"] else 0
+
+    if bbox is not None and area > 1 and segmentation is not None:
+
+        segmentation = segmentation[0].ravel().tolist()
+
+        annot =  {
+            # id, category id, iscrowd, segmentation, image_id, area, bbox(x,y,w,h)
+
+            "segmentation": segmentation,
+            "iscrowd": iscrowd,
+            "area": area,
+            "image_id": image_id,
+            "bbox": bbox,
+            "category_id": category_info['id'],
+            "id": segmentation_id
+        }
+        print(annot)
+        return annot
+
+    else:
+        return None
+
+
+def dataset_to_coco(root_folder, info, licenses, categories, seqs, out_name):
+
+
     coco_output = {
-        "info": INFO,
-        "licenses": LICENSES,
-        "categories": CATEGORIES,
+        "info": info,
+        "licenses": licenses,
+        "categories": categories,
         "images": [],
         "annotations": []
     }
 
+    images_folder = os.path.join(root_folder, "images")
+    annotation_folder = os.path.join(root_folder, "masks")
     image_id = 1
     segmentation_id = 1
 
-    for file in os.listdir(IMAGES_DIR):
-        im_file = os.path.join(IMAGES_DIR, file)
+    for file in os.listdir(images_folder):
+        if file[0:3] not in seqs:
+            continue
+        im_file = os.path.join(images_folder, file)
         im = Image.open(im_file)
 
-        im_info = pycococreatortools.create_image_info(image_id, os.path.basename(file), im.size)
+        im_info = get_image_coco_info(image_id, os.path.basename(file), im.size)
         coco_output["images"].append(im_info)
 
         masks = get_mask_images(file)
 
         for mask in masks:
-            mask_file = os.path.join(ANNOTATIONS_DIR, mask)
+            mask_file = os.path.join(annotation_folder, mask)
 
             class_id = [x['id'] for x in CATEGORIES if x['name'] in mask_file][0]
             category_info = {'id': class_id, 'is_crowd': 'crowd' in mask_file}
             binary_mask = np.asarray(Image.open(mask_file).convert('1')).astype(np.uint8)
 
-            annot_info = pycococreatortools.create_annotation_info(segmentation_id, image_id, category_info,
-                                                                   binary_mask, im.size, tolerance=1)
+            annot_info = get_annotations_coco_image(segmentation_id, image_id, category_info, binary_mask)
 
             if annot_info is not None:
+
                 coco_output["annotations"].append(annot_info)
 
             segmentation_id = segmentation_id + 1
 
         image_id = image_id + 1
 
-    print(coco_output)
+    with open(os.path.join(ROOT_DIR, out_name), "w") as f:
+        json.dump(coco_output, f)
 
 
 def rename_images():
@@ -139,9 +193,11 @@ def rename_images():
         # im_number = int(im.split(".")[0])
         # new_name = "{:03d}.{}".format(im_number,extension)
 
-        os.rename(os.path.join(IMAGES_DIR, im), os.path.join(IMAGES_DIR, new_name))
+        # os.rename(os.path.join(IMAGES_DIR, im), os.path.join(IMAGES_DIR, new_name))
 
 
 if __name__ == '__main__':
-    # rename_images()
-    cocoize_dataset()
+    train_seq = ["{:03d}".format(x) for x in range(1,16)]
+    val_seq= ["{:03d}".format(x) for x in range(16, 19)]
+    dataset_to_coco(ROOT_DIR, INFO, LICENSES, CATEGORIES, train_seq, "train.json")
+    dataset_to_coco(ROOT_DIR, INFO, LICENSES, CATEGORIES, val_seq, "val.json")
