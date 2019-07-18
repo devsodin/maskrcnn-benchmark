b28463485d5b6d6954edf3cb77b341b39d399ec7
Yael Tudela <devsodin@gmail.com>
2019-07-18 11:22:47 +0200

    challenge validation added (localization)

--- tools/challenge_validation.py
+++ tools/challenge_validation.py
@@ -5,6 +5,7 @@ from PIL import Image
 from matplotlib import pyplot as plt
 from scipy.ndimage.measurements import label
 
+
 def polyp_detection_results(csv_folder, output_folder, gt_folder, threshold=0.0):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -88,7 +89,7 @@ def process_video(file, has_confidence, thresh, vid_folder):
 
     rt = first_detected_polyp - first_polyp if first_detected_polyp != -1 else -1
 
-    return fn, fp, rt, tn, tp, video_pred, video_gt
+    return fn, fp, rt, tn, tp, video_gt, video_pred
 
 
 def save_detection_plot(output_folder, threshold, vid_folder, video_gt, video_pred):
@@ -126,8 +127,10 @@ def polyp_localization_challenge(csv_folder, output_folder, gt_folder, threshold
             im_frame_np = np.asarray(im_frame)
             is_polyp = im_frame_np.sum() > 0
 
-
-            labeled_frame, max_polyp = label(im_frame)
+            kernel = np.array([[1, 1, 1],
+                               [1, 1, 1],
+                               [1, 1, 1]])
+            labeled_frame, max_polyp = label(im_frame, structure=kernel)
 
             if is_polyp and first_polyp == -1:
                 first_polyp = polyp_n
@@ -138,23 +141,21 @@ def polyp_localization_challenge(csv_folder, output_folder, gt_folder, threshold
 
             if frame_output.empty:
                 if is_polyp:
-                    fn += 1
+                    fn += max_polyp
                 else:
                     tn += 1
             else:
                 already_detected = []
 
-                for detection in frame_output.iterrows():
-                    detection = detection[1]
+                for detection_row in frame_output.iterrows():
+                    detection = detection_row[1]
                     frame_pred = True
-                    centroid_x = int(detection[2])
-                    centroid_y = int(detection[1])
+                    centroid_x = int(round(detection[2]))
+                    centroid_y = int(round(detection[1]))
 
                     if frame_pred:
                         if is_polyp:
-                            print(im_frame_np.shape)
-                            if im_frame_np[centroid_x, centroid_y] == 1:
-                                print(labeled_frame[centroid_x, centroid_y])
+                            if im_frame_np[centroid_x, centroid_y] == 255:
                                 if labeled_frame[centroid_x, centroid_y] not in already_detected:
                                     tp += 1
                                     already_detected += [labeled_frame[centroid_x, centroid_y]]
@@ -176,6 +177,11 @@ def polyp_localization_challenge(csv_folder, output_folder, gt_folder, threshold
 
         print(vid_name, [tp, fp, fn, tn, rt], len(sorted(os.listdir(vid_folder))), sum([tp, fp, fn, tn, rt]))
 
+        res_dict[vid_name] = [tp, fp, fn, tn, rt]
+
+    pd.DataFrame.from_dict(res_dict, orient="index", columns=["TP", "FP", "FN", "TN", "RT"]).to_csv(
+        output_folder + "/lres{}.csv".format(threshold), index=False)
+
 
 if __name__ == '__main__':
     detection_csv = pd.DataFrame(
@@ -186,5 +192,5 @@ if __name__ == '__main__':
         #                         "/home/yael/Downloads/PolypDetectionTest/GT", threshold=threshold / 10)
 
         polyp_localization_challenge("/home/yael/Downloads/challenge_CVC_2/challenge/Localization/CVC2/",
-                                "/home/yael/Downloads/challenge_CVC_2/challenge/res_yael/Localization/CVC2/",
-                                "/home/yael/Downloads/PolypDetectionTest/GT", threshold=threshold / 10)
+                                     "/home/yael/Downloads/challenge_CVC_2/challenge/res_yael/Localization/CVC2/",
+                                     "/home/yael/Downloads/PolypDetectionTest/GT", threshold=threshold / 10)
